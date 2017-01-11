a014a0dfbb363aa87e1c636b99a17c17ede52191
Ben Barrett <barrettb@pvipanther.net>
2017-01-11 17:57:54 -0500

    Hello World v6

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -6,6 +6,9 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 
 	}
 }
