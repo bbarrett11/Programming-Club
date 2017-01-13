77ae7164c26d7516d30111f5f20801a9d32416ba
pviparent <pviparent@ConleyA.paulvi.net>
2017-01-13 07:47:42 -0500

    HEAR MY WORDS BEN

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -9,6 +9,8 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 		System.out.println("Hello World");
+		
+		System.out.println("SEE MY WORDS BEN");
 
 	}
 }
