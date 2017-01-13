00a007be8fee0fcbc9f7782914e88e1525fd773f
Ben Barrett <barrettb@pvipanther.net>
2017-01-13 08:25:42 -0500

    How to Comment

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -11,6 +11,17 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		//hello?
 		System.out.println("SEE MY WORDS BEN");
-
+		System.out.println(howToAddComments(1));
+	}
+	
+	/**
+	 * A method that returns an explanation on how to write java-Docs for methods
+	 * @param uselessParameter a useless parameter that exists only to illustrate
+	 * @return A Didatic String that explains how good comments are created.
+	 */
+	public static String howToAddComments(int uselessParameter)
+	{
+		return "To add the ninfty thing above this method, simply type - \"/**\" above the method you wish to explaine and press enter\n"
+				+"To create in line comments, put \"\\\\\" next to the thing you are describing.";
 	}
 }
