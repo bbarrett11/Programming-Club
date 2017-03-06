5d35577620117029594430884079e71d08179b1a
pviparent <pviparent@ConleyA.paulvi.net>
2017-03-06 12:08:46 -0500

    put empty strings into character creater unit initialization to stop the error messages

--- src/RPG/CharacterCreator.java
+++ src/RPG/CharacterCreator.java
@@ -4,9 +4,9 @@ import java.io.*;
 
 public class CharacterCreator implements Serializable {
 	public static void main(String[] args) throws IOException {
-		Unit hope = new Unit("axeBandit", "bandit", 1, "swordSword", 15, 4, 2, 0, 8, 2, -5, 4);
+		Unit hope = new Unit("axeBandit", "bandit", 1, "swordSword", 15, 4, 2, 0, 8, 2, -5, 4, "");
 
-		Unit senorSavesTheDay = new Unit("SenorSavesTheDay", "Hero", 0, "swordSword of Legends", 15, 4, 2, 0, 8, 2, -5, 4);
+		Unit senorSavesTheDay = new Unit("SenorSavesTheDay", "Hero", 0, "swordSword of Legends", 15, 4, 2, 0, 8, 2,-5,4,"");
 		makeCharacter(senorSavesTheDay, "Saves\\Roster\\");
 	}
 	
